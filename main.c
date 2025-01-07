33fd1727c2211eeb5c0429ef0f7f966a1cb8dbc2
1stub <stubbiegaming@gmail.com>
2025-01-07 11:12:51 -0500

    moved header and data seg to own contig block in the page

--- main.c
+++ main.c
@@ -16,17 +16,19 @@
 #define NUM_BLOCKS_PER_PAGE     PAGE_SIZE / BLOCK_SIZE
 
 //actual data in our page, will be 24 bytes
-typedef struct block_head{
-    struct block_head* next;
-    size_t size;
-}block_head;
+typedef struct block_header{
+    struct block_header* next;
+    
+    //pointer to data segment of this block
+    void* data;
+}block_header;
 
 //our big chunk of contiguous memory
 typedef struct Page{
     uint8_t free_list[NUM_BLOCKS_PER_PAGE];
     struct Page* next;
-    struct block_head* block_list_head;
-    struct block_head* block_list_tail;
+    struct block_header* block_list_head;
+    struct block_header* block_list_tail;
 } Page;
 
 //Allocates a 4k page with NUM_BLOCKS_PER_PAGE supported number of blocks.
@@ -43,14 +45,14 @@ Page* allocate_page() {
     assert(p_ptr != MAP_FAILED);
 
     //pointer to first element of block list
-    p_ptr->block_list_head = (block_head*)((char*)p_ptr + sizeof(Page));
+    p_ptr->block_list_head = (block_header*)((char*)p_ptr + sizeof(Page));
 
     //tail is what we use to keep track of where we are in the page
     p_ptr->block_list_tail = p_ptr->block_list_head;
 
 #ifdef DEBUG_LOG
-    printf("Size of block_head %i\n", sizeof(block_head));
-    printf("Number of bytes of data per block %i\n", (int)(BLOCK_SIZE - sizeof(block_head)));
+    printf("Size of block_header %i\n", sizeof(block_header));
+    printf("Number of bytes of data per block %i\n", (int)(BLOCK_SIZE - sizeof(block_header)));
 #endif
 
     //initialize our page, all blocks to free
@@ -65,6 +67,7 @@ Page* allocate_page() {
 
     return p_ptr;
 }
+
 
 void* fool_alloc(Page* p, size_t size) {
     if(((char*)p->block_list_head + (int)size) - (char*)(p->block_list_head) > PAGE_SIZE){
@@ -82,8 +85,9 @@ void* fool_alloc(Page* p, size_t size) {
         return NULL;
     }
 
-    block_head* head = p->block_list_tail; //ptr to head of block list
-    int n_blocks = (size + BLOCK_SIZE - 1) / BLOCK_SIZE; //round up to nearest block size
+    block_header* head = p->block_list_tail; //ptr to the last block allocd
+    int n_blocks = (size + (BLOCK_SIZE - sizeof(block_header)) - 1) 
+        / (BLOCK_SIZE - sizeof(block_header)); //round up to nearest block size
 
 #ifdef DEBUG_LOG
     printf("For size %i data needed %i blocks\n", (int)size, n_blocks);
@@ -118,56 +122,67 @@ void* fool_alloc(Page* p, size_t size) {
 
     //now that we have found a chunk of space fitting our size we can actually
     //link the blocks together
-    for(int i = 0; i < n_blocks - 1; i++){
+    void* data_bp = 
+        (char*)p->block_list_head + (NUM_BLOCKS_PER_PAGE * sizeof(block_header));
+    block_header* last_block;
+    block_header* first_block;
+    for(int i = 0; i < n_blocks; i++){
         p->free_list[start_index + i] = 0;
-        block_head* b = (block_head*)((char*)head + ((start_index + i) * BLOCK_SIZE));
-        b->size = size; 
-        b->next = (block_head*)((char*)b + BLOCK_SIZE);
+        block_header* b = (block_header*)((char*)head + ((start_index + i) * BLOCK_SIZE));
+        
+        if(i == 0){
+            first_block = b;
+        
+        }
+        b->data = (char*)data_bp + (i * (BLOCK_SIZE - sizeof(block_header)));
+        b->next = (block_header*)((char*)b + sizeof(block_header));
 
 #ifdef DEBUG_LOG
-    printf("Allocated block starting at %p\n", b);
+    printf("Allocated block header starting at %p\n", b);
+    printf("Allocated block data segment starting at %p\n", b->data);
 #endif
 
         p->block_list_tail = b;
-    }
 
-    block_head* last_block = (block_head*)((char*)head + (n_blocks * BLOCK_SIZE));
-    last_block->size = size;
-    last_block->next = (block_head*)((char*)last_block + BLOCK_SIZE);
+        if(i == (n_blocks - 1)){
+            last_block = b;
+        }
+    }
 
     p->block_list_tail = last_block->next;
 
 #ifdef DEBUG_LOG
-    printf("Final block starting at %p\n", last_block); 
     printf("FINSHED ALLOCATING OBJECT\n\n");
 #endif
 
-    //maybe return a pointer to data seg just after header?
-    return (void*)head;
+    return (void*)(first_block->data);
 }
 
-//our block isnt technically free, however we remove the flag s.t. new data
-//can be written in data segment of block
-void fool_free(Page* p, void* obj) {
-    //we need to know how many bytes we have allocated for our
-    //sequence of blocks in order ot free properly.
 
-    block_head* block = (block_head*)obj;
-    int offset = (char*)obj - (char*)p;
+void fool_free(Page* p, void* obj) {
+    //we will be passed a pointer to the data segment of our block
+    //this will need to be converted back into block in the header 
+    //portion of our page
+
+    /*
+     
+    int offset = (char*)obj - (char*)(p->block_list_head);
     int start_index = offset / BLOCK_SIZE;
 
     //how many blocks are associated with given object
-    int allocated_blocks = ((block_head*)obj)->size / BLOCK_SIZE;
+    int allocated_blocks = ((block_header*)obj)->size / BLOCK_SIZE;
     
     while(allocated_blocks){
         p->free_list[start_index + allocated_blocks] = 1;
         allocated_blocks--;
     }
 
+    */
 #ifdef DEBUG_LOG
-    printf("Freed block at: %p\n", block);
+   // printf("Freed block at: %p\n", block);
 #endif
 }
+
 
 void destroy_page(Page* p) {
 
@@ -178,15 +193,23 @@ void destroy_page(Page* p) {
     munmap(p, PAGE_SIZE);
 }
 
+
 int main(){
     Page* page = allocate_page();
    
     //we are trying to allocate one 64 byte chunk and another
     //128 byte chunk
+    //then 8192 byte block which will fail (for now) since we 
+    //exceede a page size
     void* object_one = fool_alloc(page, 64);
     void* object_two = fool_alloc(page, 128);
 
-    void* failed_obj = fool_alloc(page, 8192);
+    void* failed_alloc = fool_alloc(page, 8192);
+
+    int* data = (int*)object_one;
+    *data = 0x42;
+
+    printf("Data stored at object one: %i\n", *data);
 
     //if either are null we should make a new page - fine for now
     if(object_one != NULL && object_two != NULL){
