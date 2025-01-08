8281de0c6694e35f402b503ca55287e70c5660d5
Mark Marron <mark.marron@protonmail.com>
2025-01-07 23:25:50 -0500

    yes

--- src/runtime/memory/common.h
+++ src/runtime/memory/common.h
@@ -3,3 +3,45 @@
 #include <stdint.h>
 #include <assert.h>
 #include <sys/mman.h> //mmap
+
+//DEFAULT ENABLED WHILE LOTS OF DEVELOPMENT!!!!
+#define BSQ_GC_CHECK_ENABLED
+#define MEM_STATS
+
+#ifdef BSQ_GC_CHECK_ENABLED
+#define ALLOC_DEBUG_STW_GC
+#define ALLOC_DEBUG_MEM_INITIALIZE
+#define ALLOC_DEBUG_CANARY
+#endif
+
+//Can also use other values like 0xFFFFFFFFFFFFFFFFul
+#define ALLOC_DEBUG_MEM_INITIALIZE_VALUE 0x0ul
+
+//Must be multiple of 8
+#define ALLOC_DEBUG_CANARY_SIZE 16
+#define ALLOC_DEBUG_CANARY_VALUE 0xDEADBEEFDEADBEEFul
+
+#ifdef MEM_STATS
+#define ENABLE_MEM_STATS
+#define MEM_STATS_OP(X) X
+#define MEM_STATS_ARG(X) X
+#else
+#define MEM_STATS_OP(X)
+#define MEM_STATS_ARG(X)
+#endif
+
+////////////////////////////////
+//Memory allocator
+
+#define BSQ_STACK_ALLOC(SIZE) ((SIZE) == 0 ? nullptr : alloca(SIZE))
+
+//Block allocation size
+#define BSQ_BLOCK_ALLOCATION_SIZE 4096ul
+
+//Make sure any allocated page is addressable by us -- larger than 2^31 and less than 2^42
+#define MIN_ALLOCATED_ADDRESS 2147483648ul
+#define MAX_ALLOCATED_ADDRESS 281474976710656ul
+
+typedef struct block_header{
+    
+} block_header;
