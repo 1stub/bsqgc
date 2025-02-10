a89f74ef1b2a161999a1d0be548156880491990e
1stub <stubbiegaming@gmail.com>
2025-02-10 16:35:02 -0500

    worklist structure and makefile

---
+++ src/runtime/support/worklist.c
@@ -0,0 +1,1 @@
+#include "worklist.h"
---
+++ src/runtime/support/worklist.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "../common.h"
+
+/* This worklist is designed to be very similar to the array list but only support head operations */
+
+typedef int cdoesntlikeemptytranslationunits;
